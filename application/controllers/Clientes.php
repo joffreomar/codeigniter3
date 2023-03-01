94e543d47c3254558e51a0ca1b2392343a91c7e5
aj1_kx2 <jofre.omar.en@gmail.com>
2023-03-01 10:22:16 -0500

    Actualización excel de carga masiva de clientes

--- application/controllers/Clientes.php
+++ application/controllers/Clientes.php
@@ -99,7 +99,13 @@ class Clientes extends CI_Controller
         $archivo = $this->simplexlsx1->generate($_FILES['matriz_clientes']['tmp_name']);
         $contador = 0;
         foreach ($archivo->rows() as $fields) {
-          if (!empty(trim($fields[0])) && !empty(trim($fields[2])) && !$this->cliente->consultarClienteRepetido($fields[2])) {
+          if (
+            !empty(trim($fields[0]))
+            && !empty(trim($fields[2]))
+            && !$this->cliente->consultarClienteRepetido($fields[2])
+            && !$this->cuenta->consultarCuentaRepetida($fields[2])
+            && !$this->cuenta->consultarMedidorRepetido($fields[7])
+          ) {
             $data_cliente['cedula_cliente'] = '';
             $data_cliente['nombre_cliente'] = '';
             $data_cliente['apellido_cliente'] = '';
@@ -113,29 +119,24 @@ class Clientes extends CI_Controller
             $data_cuenta['fk_id_tpcuenta'] = 1;
             $data_cuenta['fk_id_sector'] = 1;
             if ($contador >= 1) {
-              $bandera = false;
-              if (!empty($fields[2])) {
-                $bandera = $this->cuenta->consultarCuentaRepetida($fields[2]);
-                $bandera = $this->cuenta->consultarMedidorRepetido($fields[7]);
-              }
+
               //si $bandera es falso significa que no encontró clientes repetidos, entonces guarda el cliente
-              if (!$bandera) {
-                $data_cliente['cedula_cliente'] = trim($fields[2]);
-                $data_cliente['telefono_cliente'] = trim($fields[3]);
-                $data_cliente['correo_cliente'] = trim($fields[4]);
-                $data_cliente['nombre_cliente'] = trim($fields[5]);
-                $data_cliente['apellido_cliente'] = trim($fields[6]);
-                $id = $this->cliente->insertarCargaMasiva($data_cliente);
-                $data_cuenta['numero_cuenta'] = trim($fields[1]);
-                $data_cuenta['numero_medidor_cuenta'] = trim($fields[9]);
-                $data_cuenta['direccion_primaria_cuenta'] = trim($fields[7]);
-                $data_cuenta['direccion_secundaria_cuenta'] = trim($fields[8]);
-                $data_cuenta['estado_cuenta'] = "ACTIVA";
-                $data_cuenta['fk_id_cliente'] = $id;
-                $data_cuenta['fk_id_tpcuenta'] = trim($fields[10]);
-                $data_cuenta['fk_id_sector'] = trim($fields[11]);
-                $this->cuenta->insertar($data_cuenta);
-              }
+
+              $data_cliente['cedula_cliente'] = trim($fields[2]);
+              $data_cliente['telefono_cliente'] = trim($fields[3]);
+              $data_cliente['correo_cliente'] = trim($fields[4]);
+              $data_cliente['nombre_cliente'] = trim($fields[5]);
+              $data_cliente['apellido_cliente'] = trim($fields[6]);
+              $id = $this->cliente->insertarCargaMasiva($data_cliente);
+              $data_cuenta['numero_cuenta'] = trim($fields[1]);
+              $data_cuenta['numero_medidor_cuenta'] = trim($fields[9]);
+              $data_cuenta['direccion_primaria_cuenta'] = trim($fields[7]);
+              $data_cuenta['direccion_secundaria_cuenta'] = trim($fields[8]);
+              $data_cuenta['estado_cuenta'] = "ACTIVA";
+              $data_cuenta['fk_id_cliente'] = $id;
+              $data_cuenta['fk_id_tpcuenta'] = trim($fields[10]);
+              $data_cuenta['fk_id_sector'] = trim($fields[11]);
+              $this->cuenta->insertar($data_cuenta);
             }
             $contador++;
           }
